9922c418dd185a65569a6cbc66275d729dde91bc
Stavrenas <stavrina25@gmail.com>
2021-07-14 11:50:49 +0300

    Change logo button

--- app/src/main/java/com/example/nanotypos/MainActivity.kt
+++ app/src/main/java/com/example/nanotypos/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.nanotypos
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -14,13 +15,30 @@ class MainActivity : AppCompatActivity() {
         rollButton.setOnClickListener {
             rollDice()
             Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT).show()}
+
+        val logoButton: Button = findViewById(R.id.logo)
+        var logo = true
+        logoButton.setOnClickListener{
+            val logoImage: ImageView = findViewById(R.id.logo_image)
+
+            logo = if (logo) {
+                logoImage.setImageResource(R.drawable.nanotypos_logo1)
+                false
+            } else {
+                logoImage.setImageResource(R.drawable.nanotypus)
+                true
+            }
+
+        }
     }
+
 
     private fun rollDice() {
         val dice = Dice(6)
         val diceRoll = dice.roll()
         val resultTextView: TextView = findViewById(R.id.textView)
         resultTextView.text = diceRoll.toString()
+
     }
 }
 
