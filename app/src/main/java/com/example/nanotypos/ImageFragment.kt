d424f211dbf87315cf972b5c81ee52075d7d74fb
Stavrenas <stavrina25@gmail.com>
2021-08-04 19:12:02 +0300

    added detector

--- app/src/main/java/com/example/nanotypos/ImageFragment.kt
+++ app/src/main/java/com/example/nanotypos/ImageFragment.kt
@@ -2,6 +2,7 @@ package com.example.nanotypos
 
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,9 +11,12 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import com.example.nanotypos.data.ViewModel
 import com.example.nanotypos.databinding.FragmentImageBinding
+import com.google.mlkit.common.model.LocalModel
 import com.google.mlkit.vision.barcode.Barcode
 import com.google.mlkit.vision.barcode.BarcodeScanning
 import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.objects.ObjectDetection
+import com.google.mlkit.vision.objects.custom.CustomObjectDetectorOptions
 import java.io.IOException
 
 class ImageFragment: Fragment() {
@@ -22,6 +26,10 @@ class ImageFragment: Fragment() {
     private var _binding: FragmentImageBinding? = null
 
     private val binding get() = _binding!!
+
+    val localModel = LocalModel.Builder()
+        .setAssetFilePath("ml/model.tflite")
+        .build()
 
 
     override fun onCreateView(
@@ -48,6 +56,35 @@ class ImageFragment: Fragment() {
     }
 
     fun searchForLogo(){
+        val uri: Uri? = sharedViewModel.getModelUri()
+        val image: InputImage
+        // Live detection and tracking
+        val customObjectDetectorOptions =
+            CustomObjectDetectorOptions.Builder(localModel)
+                .setDetectorMode(CustomObjectDetectorOptions.STREAM_MODE)
+                .enableClassification()
+                .setClassificationConfidenceThreshold(0.5f)
+                .setMaxPerObjectLabelCount(3)
+                .build()
+
+        val objectDetector =ObjectDetection.getClient(customObjectDetectorOptions)
+        try {
+            image = InputImage.fromFilePath(context, uri)
+            objectDetector.process(image).
+            addOnFailureListener { e ->
+
+                    Log.d("LOGO", "error is $e")
+
+            }
+            .addOnSuccessListener{results ->
+                for (detectedObject in results) {
+
+                }
+            }
+
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
         Toast.makeText(activity, "Search for Logo pressed!", Toast.LENGTH_SHORT).show()
     }
 
