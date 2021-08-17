b3ccc682dfb2886dcc04ce579e54bc1186b6189b
Stavrenas <stavrina25@gmail.com>
2021-08-17 21:05:13 +0300

    show bounding box

--- app/src/main/java/com/example/nanotypos/ImageFragment.kt
+++ app/src/main/java/com/example/nanotypos/ImageFragment.kt
@@ -1,5 +1,6 @@
 package com.example.nanotypos
 
+import android.graphics.*
 import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
@@ -20,11 +21,17 @@ import org.tensorflow.lite.support.image.TensorImage
 import org.tensorflow.lite.task.vision.detector.Detection
 import org.tensorflow.lite.task.vision.detector.ObjectDetector
 import java.io.IOException
+import java.util.*
+
 
 class ImageFragment: Fragment() {
 
     private val sharedViewModel: ViewModel by activityViewModels()
     private var binding: FragmentImageBinding? = null
+
+    companion object {
+        private const val MAX_FONT_SIZE = 96F
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -99,13 +106,14 @@ class ImageFragment: Fragment() {
     */
 
 
-    fun searchForLogo (){
+    fun searchForLogo() {
         val uri: Uri? = sharedViewModel.getModelUri()
         //val image: InputImage
 
         // Initialization
         val options: ObjectDetector.ObjectDetectorOptions =
-            ObjectDetector.ObjectDetectorOptions.builder().setMaxResults(1).build()
+            ObjectDetector.ObjectDetectorOptions.builder().setMaxResults(1).setScoreThreshold(0.1f).build()
+
         val objectDetector: ObjectDetector =
             ObjectDetector.createFromFileAndOptions(context, "model.tflite", options)
         try {
@@ -115,24 +123,84 @@ class ImageFragment: Fragment() {
             // Run inference
             val results: List<Detection> = objectDetector.detect(tensorImage)
             for (detectedObject in results) {
+
                 val boundingBox = detectedObject.boundingBox
-                Log.d("LOGO", " boundingBox: (${boundingBox.left}, ${boundingBox.top}) - (${boundingBox.right},${boundingBox.bottom})")
-                Toast.makeText(activity," boundingBox: (${boundingBox.left}, ${boundingBox.top}) - (${boundingBox.right},${boundingBox.bottom})", Toast.LENGTH_SHORT).show()
-                for (category in detectedObject.categories){
+
+                Log.d(
+                    "LOGO",
+                    " boundingBox: (${boundingBox.left}, ${boundingBox.top}) - (${boundingBox.right},${boundingBox.bottom})"
+                )
+                Toast.makeText(
+                    activity,
+                    " boundingBox: (${boundingBox.left}, ${boundingBox.top}) - (${boundingBox.right},${boundingBox.bottom})",
+                    Toast.LENGTH_SHORT
+                ).show()
+
+                for (category in detectedObject.categories) {
                     val label = category.label
                     val score = category.score
-                    Log.d("LOGO", "Label is $label")
-                    Log.d("LOGO", "Score is $score")
-                    Toast.makeText(activity, "Label is $label", Toast.LENGTH_SHORT).show()
-                    Toast.makeText(activity,"Score is $score", Toast.LENGTH_SHORT).show()
+                    Log.d("LOGO", "Label is $label and score is $score ")
+                    Toast.makeText(
+                        activity,
+                        "Label is $label and score is $score",
+                        Toast.LENGTH_SHORT
+                    ).show()
+
+                    val imgWithResult = drawDetectionResult(bitmap, detectedObject)
+                    binding?.targetImage?.setImageBitmap(imgWithResult)
+                    //sharedViewModel.setModelUri(imgWithResult)
                 }
 
             }
-            //val result = objectDetector.detect(tensorImage)
-
         } catch (e: IOException) {
             e.printStackTrace()
         }
+    }
+
+    private fun drawDetectionResult(
+        bitmap: Bitmap,
+        detectionResult: Detection
+    ): Bitmap {
+        val outputBitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true)
+        val canvas = Canvas(outputBitmap)
+        val pen = Paint()
+        pen.textAlign = Paint.Align.LEFT
+
+            // draw bounding box
+            pen.color = Color.RED
+            pen.strokeWidth = 8F
+            pen.style = Paint.Style.STROKE
+            val box = detectionResult.boundingBox
+            canvas.drawRect(box, pen)
+
+
+            val tagSize = Rect(0, 0, 0, 0)
+
+            // calculate the right font size
+            pen.style = Paint.Style.FILL_AND_STROKE
+            pen.color = Color.YELLOW
+            pen.strokeWidth = 2F
+
+            pen.textSize = MAX_FONT_SIZE
+                for (category in detectionResult.categories) {
+                val label = category.label
+                val score = category.score
+                    pen.getTextBounds(label, 0, label.length, tagSize)
+
+                    val fontSize: Float = pen.textSize * box.width() / tagSize.width()
+
+                    // adjust the font size so texts are inside the bounding box
+                    if (fontSize < pen.textSize) pen.textSize = fontSize
+
+                    var margin = (box.width() - tagSize.width()) / 2.0F
+                    if (margin < 0F) margin = 0F
+                    canvas.drawText(
+                        label, box.left + margin,
+                        box.top + tagSize.height().times(1F), pen
+                    )
+            }
+
+        return outputBitmap
     }
 
 
@@ -158,7 +226,7 @@ class ImageFragment: Fragment() {
 
      */
 
-    fun searchForQR(){
+    fun searchForQR() {
         val uri: Uri? = sharedViewModel.getModelUri()
         val image: InputImage
         try {
@@ -169,42 +237,42 @@ class ImageFragment: Fragment() {
             //Process the image
             //val result = scanner...
             scanner.process(image).addOnSuccessListener { barcodes ->
-                    for (barcode in barcodes) {
-                        //Get information from barcodes
+                for (barcode in barcodes) {
+                    //Get information from barcodes
 
-                        //val bounds = barcode.boundingBox
-                        //val corners = barcode.cornerPoints
-                        //val rawValue = barcode.rawValue
+                    //val bounds = barcode.boundingBox
+                    //val corners = barcode.cornerPoints
+                    //val rawValue = barcode.rawValue
 
-                        // See API reference for complete list of supported types
-                        when (barcode.valueType) {
-                            Barcode.TYPE_WIFI -> {
-                                //val ssid = barcode.wifi!!.ssid
-                                //val password = barcode.wifi!!.password
-                                //val type = barcode.wifi!!.encryptionType
-                            }
-                            Barcode.TYPE_URL -> {
-                                //val title = barcode.url!!.title
-                                //val url = barcode.url!!.url
-                                sharedViewModel.setBarcode(barcode)
-                                sharedViewModel.setTextValue(getString(R.string.QR_success))
-                                foundCode = true
-                                findNavController().navigate(R.id.action_imageFragment_to_successFragment)
-                            }
-                            Barcode.TYPE_TEXT ->{
-                                sharedViewModel.setBarcode(barcode)
-                                sharedViewModel.setTextValue(getString(R.string.QR_failure))
-                                findNavController().navigate(R.id.action_imageFragment_to_successFragment)
-                            }
+                    // See API reference for complete list of supported types
+                    when (barcode.valueType) {
+                        Barcode.TYPE_WIFI -> {
+                            //val ssid = barcode.wifi!!.ssid
+                            //val password = barcode.wifi!!.password
+                            //val type = barcode.wifi!!.encryptionType
+                        }
+                        Barcode.TYPE_URL -> {
+                            //val title = barcode.url!!.title
+                            //val url = barcode.url!!.url
+                            sharedViewModel.setBarcode(barcode)
+                            sharedViewModel.setTextValue(getString(R.string.QR_success))
+                            foundCode = true
+                            findNavController().navigate(R.id.action_imageFragment_to_successFragment)
+                        }
+                        Barcode.TYPE_TEXT -> {
+                            sharedViewModel.setBarcode(barcode)
+                            sharedViewModel.setTextValue(getString(R.string.QR_failure))
+                            findNavController().navigate(R.id.action_imageFragment_to_successFragment)
                         }
                     }
-                    // Task completed successfully
-                    // ...
-
-                    //Display toast if QR is not found
-                    if(!foundCode)
-                        Toast.makeText(activity, "No QR code found", Toast.LENGTH_SHORT).show()
                 }
+                // Task completed successfully
+                // ...
+
+                //Display toast if QR is not found
+                if (!foundCode)
+                    Toast.makeText(activity, "No QR code found", Toast.LENGTH_SHORT).show()
+            }
                 .addOnFailureListener {
                     // Task failed with an exception
                     // ...
